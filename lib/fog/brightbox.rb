7a56484817314f241a79927dd8711e690e15c728
Paul Thornthwaite <tokengeek@gmail.com>
2015-06-03 12:59:57 +0100

    Fix URL reference to main fog repo

--- lib/fog/brightbox.rb
+++ lib/fog/brightbox.rb
@@ -1,6 +1,6 @@
 # See the main fog gem for more details about the top level class {Fog}
 #
-# @see https://github.com/fog/fo://github.com/fog/fog
+# @see https://github.com/fog/fog
 # @see http://fog.io/
 # @see http://rubydoc.info/gems/fog
 #
