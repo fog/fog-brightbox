792acbb0db2247ff718462659cc8146647703b61
Malo Skrylevo <majioa@yandex.ru>
2018-08-31 13:41:29 +0300

    Migrated from abandoned inflecto gem in favor of dry-inflector gem

--- lib/fog/brightbox/model_helper.rb
+++ lib/fog/brightbox/model_helper.rb
@@ -1,14 +1,16 @@
-require "inflecto"
+require "dry/inflector"
 
 module Fog
   module Brightbox
     module ModelHelper
       def resource_name
-        Inflecto.underscore(Inflecto.demodulize(self.class))
+        inflector = Dry::Inflector.new
+
+        inflector.underscore(inflector.demodulize(self.class))
       end
 
       def collection_name
-        Inflecto.pluralize(resource_name)
+        Dry::Inflector.new.pluralize(resource_name)
       end
     end
   end
