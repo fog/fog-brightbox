6a903537be496b0f4747bd1cd77baa916c12d360
Paul Thornthwaite <tokengeek@gmail.com>
2023-01-24 14:54:43 +0000

    Update `ConfigMap` code
    This adds the `#save` and `#destroy` methods which are required by the
    Brightbox CLI to enable adding commands to manage them.

--- lib/fog/brightbox/models/compute/config_map.rb
+++ lib/fog/brightbox/models/compute/config_map.rb
@@ -8,6 +8,38 @@ module Fog
 
         attribute :name
         attribute :data
+
+        def save
+          options = {
+            name: name
+          }.delete_if { |_k, v| v.nil? }
+
+          payload = JSON.decode(attributes[:data])
+
+          res = if persisted?
+                  # update
+                  options[:data] = payload
+                  service.update_config_map(identity, options)
+                else
+                  # create
+                  raise Fog::Errors::Error.new("'data' is required") if data.nil? || data.empty?
+                  options[:data] = payload
+                  service.create_config_map(options)
+                end
+
+          merge_attributes(res)
+          true
+        rescue StandardError => e
+          raise Fog::Errors::Error.new(e.message)
+          false
+        end
+
+        def destroy
+          requires :identity
+          res = service.delete_config_map(identity)
+          merge_attributes(res)
+          true
+        end
       end
     end
   end
